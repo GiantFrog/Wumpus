90ee4fa9d5e154ec363106a73e09f52305148d45
GiantFrog <35314154+GiantFrog@users.noreply.github.com>
2018-12-01 13:03:10 -0700

    Add basic controls and prolog assertions when exploring spaces

---
+++ src/WumpusDriver.java
@@ -0,0 +1,11 @@
+public class WumpusDriver
+{
+	public static void main (String[] args)
+	{
+		WumpusWorld world = new WumpusWorld(4);
+	}
+	//TODO implement goTo w/ maze algorithm using safe spaces
+	//TODO main menu with level size chooser
+	//TODO random level gen (1 wumpus, 1 gold, (1,1) is always clear, 20% chance of pit on other tiles)
+	//TODO write prolog rules
+}
--- src/WumpusWorld.java
+++ src/WumpusWorld.java
@@ -2,52 +2,27 @@ import org.jpl7.*;
 
 public class WumpusWorld
 {
-	public static void main (String[] args)
+	private Room[][] map;
+	private int agentX, agentY, agentDirection, moves, points;
+	
+	public WumpusWorld (int mapSize)
 	{
-		/*
-		Query testQuery = new Query("consult", new Term[] {new Atom("test.pl")});
-		System.out.println( "consult " + (testQuery.hasSolution() ? "succeeded" : "failed"));
-		Query q = new Query("child_of", new Term[] {new Atom("joe"), new Atom("ralf")});
-		System.out.println("child of joe and ralf is " + (q.hasSolution() ? "provable" : "not provable"));
-		
-		Query q3 =
-				new Query(
-						"descendent_of",
-						new Term[] {new Atom("steve"),new Atom("ralf")}
-				);
-		System.out.println(
-				"descendent_of(joe,ralf) is " +
-						( q3.hasSolution() ? "provable" : "not provable" )
-		);
-		
-		Variable X = new Variable("X");
-		Query q4 =
-				new Query(
-						"descendent_of",
-						new Term[] {X,new Atom("ralf")}
-				);
-		
-		java.util.Map<String,Term> solution;
-		
-		solution = q4.oneSolution();
-		
-		System.out.println( "first solution of descendent_of(X, ralf)");
-		System.out.println( "X = " + solution.get("X"));
-		*/
 		//load up the rules in wumpus_writs.pl using swi-prolog's JPL.
 		//You may need to add JPL as a library for this to work. Get it from jpl7.org
 		Query loadWrits = new Query("consult", new Term[] {new Atom("wumpus_writs.pl")});
-		generateMap(4);
+		System.out.println( "Wumpus Writs have been loaded" + (loadWrits.hasSolution() ? " successfully!" : "... Not at all. They failed to load."));
+		
+		points = moves = 0;
+		agentX = agentY = agentDirection = 1;
+		map = generateMap(mapSize);
+		sendSenses(agentX, agentY);
 	}
-	//TODO implement goTo w/ maze algorithm using safe spaces
-	//TODO main menu with level size chooser
-	//TODO random level gen (1 wumpus, 1 gold, (1,1) is always clear, 20% chance of pit on other tiles)
-	//TODO write prolog rules
 	
 	//returns a random map of the specified dimensions
-	public static Room[][] generateMap (int size)
+	//Every map contains a wumpus, a golden treasure, and some bottomless pits.
+	private Room[][] generateMap (int size)
 	{
-		Room[][] map = new Room[size+2][size+2];	//we add 2 to account for walls
+		map = new Room[size+2][size+2];	//we add 2 to account for walls
 		
 		//initialize all the rooms as empty
 		for (int a = 0; a < size+2; a++)
@@ -60,12 +35,12 @@ public class WumpusWorld
 		for (int a = 0; a < size+2; a++)
 		{
 			map[0][a].setWall();
-			map[size+2][a].setWall();
+			map[size+1][a].setWall();
 		}
-		for (int a = 1; a <= size+2; a++)
+		for (int a = 1; a < size+2; a++)
 		{
 			map[a][0].setWall();
-			map[a][size+2].setWall();
+			map[a][size+1].setWall();
 		}
 		
 		//add a wumpus and a gold to any tile that isn't 1, 1
@@ -78,6 +53,7 @@ public class WumpusWorld
 			{
 				if (goldAdded)
 				{
+					System.out.println("The Wumpus is in " + j + ", " + k);
 					map[j][k].setWumpus();
 					map[j][k].setStinky();
 					map[j+1][k].setStinky();
@@ -88,6 +64,7 @@ public class WumpusWorld
 				}
 				else
 				{
+					System.out.println("The gold is in " + j + ", " + k);
 					map[j][k].setGold();
 					map[j][j].setGlittery();
 					goldAdded = true;
@@ -103,6 +80,7 @@ public class WumpusWorld
 			{
 				if (!atOneOne && !map[a][b].isGold() && !map[a][b].isWall() && (int)(Math.random()*5) == 0)
 				{
+					System.out.println("A pit is in " + a + ", " + b);
 					map[a][b].setPit();
 					map[a][b].setDrafty();
 					map[a+1][b].setDrafty();
@@ -114,6 +92,76 @@ public class WumpusWorld
 					atOneOne = false;
 			}
 		}
+		
+		//this helps prolog recognize room adjacency
+		for (int a = 1; a < size+2; a++)
+		{
+			new Query("assert(nextTo(" + a + "," + (a-1) + "))").hasSolution();
+			new Query("assert(nextTo(" + (a-1) + "," + a + "))").hasSolution();
+		}
 		return map;
+	}
+	
+	//informs the agent about the room it is currently in.
+	//returns false if the agent tried to walk into a wall.
+	private boolean sendSenses (int a, int b)
+	{
+		if (map[a][b].isStinky())
+			new Query("assert(stinky(" + a + "," + b + "))").hasSolution();
+		if (map[a][b].isDrafty())
+			new Query("assert(drafty(" + a + "," + b + "))").hasSolution();
+		if (map[a][b].isGlittery())
+			new Query("assert(glittery(" + a + "," + b + "))").hasSolution();
+		
+		if (map[a][b].isWall())
+		{
+			new Query("assert(wall(" + a + "," + b + "))").hasSolution();
+			return false;
+		}
+		else	//only count a space as explored if there's no wall
+		{
+			new Query("assert(explored(" + a + "," + b + "))").hasSolution();
+			return true;
+		}
+	}
+	
+	//moves the agent in the direction they are facing.
+	//no movement will happen if the agent tries to walk into a wall.
+	private void forward()
+	{
+		moves++;
+		points--;
+		switch (agentDirection)
+		{
+			case 0:		//north
+				if (sendSenses(agentX, agentY+1))
+					agentY++;
+				break;
+			case 1:		//east
+				if (sendSenses(agentX+1, agentY))
+					agentX++;
+				break;
+			case 2:		//south
+				if (sendSenses(agentX, agentY-1))
+					agentY--;
+				break;
+			default:	//west
+				if (sendSenses(agentX-1, agentY))
+					agentX--;
+		}
+	}
+	
+	private void turnRight()
+	{
+		moves++;
+		agentDirection++;
+		agentDirection %= 4;
+	}
+	private void turnLeft()
+	{
+		moves++;
+		agentDirection--;
+		if (agentDirection < 0)
+			agentDirection = 3;
 	}
 }
