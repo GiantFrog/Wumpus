8ce8a9b85a80ed150a5a3c9c05e8bb3ba9be3beb
GiantFrog <35314154+GiantFrog@users.noreply.github.com>
2018-12-04 23:51:06 -0700

    This thing doesn't work, and never will as long as the prolog engine can't be restarted

--- src/WumpusBenchmark.java
+++ src/WumpusBenchmark.java
@@ -3,20 +3,21 @@ public class WumpusBenchmark
 	public static void main (String[] args)
 	{
 		benchmark(4);
-		benchmark(5);
-		benchmark(8);
-		benchmark(10);
+		//benchmark(5);
+		//benchmark(8);
+		//benchmark(10);
 	}
 	
-	//runs 1000 wumpus worlds of the given size and prints the average results
+	//runs 100 wumpus worlds of the given size and prints the average results
+	//this doesn't work. prolog never resets and just get bogged down by all the rules added every run...
 	public static void benchmark (int size)
 	{
-		System.out.println("Running 1,000 " + size + "x" + size + " wumpus worlds...");
+		System.out.println("Running 100 " + size + "x" + size + " wumpus worlds...");
 		int totalActions = 0, totalPoints = 0, totalWins = 0;
 		WumpusWorld world;
 		
 		long startTime = System.currentTimeMillis();
-		for (int i = 0; i < 1000; i++)
+		for (int i = 0; i < 100; i++)
 		{
 			world = new WumpusWorld(size);
 			while (!world.agentHasDied() && !world.agentHasWon())
@@ -28,10 +29,10 @@ public class WumpusBenchmark
 		}
 		long endTime = System.currentTimeMillis();
 		
-		System.out.println((totalWins/10) + "% of games were won.");
+		System.out.println(totalWins + "% of games were won.");
 		System.out.println((endTime - startTime) + "ms to complete 1000 levels.");
-		System.out.println(((endTime - startTime)/1000) + "ms average per level.");
-		System.out.println(totalActions + " total actions made, " + (totalActions/1000) + " average.");
-		System.out.println(totalPoints + " total score, " + (totalPoints/1000) + " average.\n");
+		System.out.println(((endTime - startTime)/100) + "ms average per level.");
+		System.out.println(totalActions + " total actions made, " + (totalActions/100) + " average.");
+		System.out.println(totalPoints + " total score, " + (totalPoints/100) + " average.\n");
 	}
 }
